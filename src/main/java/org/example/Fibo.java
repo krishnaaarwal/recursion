c7875278dd504f3e13e8badf76271287d3997432
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-12 00:32:23 +0530

    Lect 1 : Understanding basics - Fibo and B.S.

---
+++ src/main/java/org/example/Fibo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Fibo {
+    public static void main(String[] args) {
+        int ans = fibo(10);
+        System.out.println(ans);
+    }
+
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-2) + fibo(n-1);
+    }
+}
