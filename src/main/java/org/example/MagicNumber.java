10f6620e17ea1c254dc731dfedffa00021de4cfb
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-15 23:51:24 +0530

    Bitwise Learning , feat: Last bit and magic number

---
+++ src/main/java/org/example/MagicNumber.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class MagicNumber {
+    public static void main(String[] args) {
+
+
+    int n = 6;
+
+    int ans = 0;
+    int base = 5;
+
+    while(n>0)
+
+    {
+        int lastBit = n & 1;
+        n = n >> 1;
+
+        ans = ans + (lastBit * base);
+        base = base * 5;
+    }
+    System.out.println(ans);
+}
+}
