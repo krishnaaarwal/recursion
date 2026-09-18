3d367b850054c6d29f5afaf4262f00243cec613a
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-18 12:16:42 +0530

    Lect 2: Recursion (Level 1 ques)

---
+++ src/main/java/org/example/Recursion/ReverseNumber.java
@@ -0,0 +1,16 @@
+package org.example.Recursion;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println(reverse(78));
+    }
+    static int reverse(int n){
+        if(n<10){
+            return n;
+        }
+        int lastDigit = n%10;
+        int count = (int)(Math.log10(n));
+        int reverse = lastDigit * (int)(Math.pow(10, count));
+        return reverse + reverse(n/10);
+    }
+}
---
+++ src/main/java/org/example/Recursion/SumOfDigits.java
@@ -0,0 +1,14 @@
+package org.example.Recursion;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(1001));
+    }
+    static int sumOfDigits(int n){
+        if(n<10){
+            return n;
+        }
+
+        return n%10 + sumOfDigits(n/10);
+    }
+}
