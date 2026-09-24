517dfcaf913fb22eeb5df115f84cacdff2497054
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-24 17:27:40 +0530

    Passing in args

--- src/main/java/org/example/Recursion/SubsequenceAndStrings/SkipACharacter.java
+++ src/main/java/org/example/Recursion/SubsequenceAndStrings/SkipACharacter.java
@@ -3,8 +3,10 @@ package org.example.Recursion.SubsequenceAndStrings;
 public class SkipACharacter {
     public static void main(String[] args) {
         System.out.println(skip("baccad",'c'));
+        System.out.println(skip("baccad","",'c'));
     }
 
+    // Approach 1
     static String skip(String str,char target){
         return skip(str,target,0);
     }
@@ -22,5 +24,22 @@ public class SkipACharacter {
             //Internally character c converted to String
             return c + skip(str,target,index+1);
         }
+    }
+
+    //Approach 2
+    static String skip(String str,String ans,char target){
+        if(str.equals("")){
+            return ans;
+        }
+
+        char c = str.charAt(0);
+        if(c == target){
+            ans = ans + "";
+        }else{
+            ans = ans + c;
+        }
+
+        String newStr = str.substring(1,str.length());
+        return skip(newStr,ans,target);
     }
 }
