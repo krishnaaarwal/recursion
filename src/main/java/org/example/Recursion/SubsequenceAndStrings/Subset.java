1f51c727adeeb1b629c2323252ad40dea5a513c2
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-24 21:35:08 +0530

    Subset with arrayList return

--- src/main/java/org/example/Recursion/SubsequenceAndStrings/Subset.java
+++ src/main/java/org/example/Recursion/SubsequenceAndStrings/Subset.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class Subset {
     public static void main(String[] args) {
-        subset("","abc",0);
+//        subset("","abc",0);
+        ArrayList<String> ans = new ArrayList<>();
+        subset("","abc",0,ans);
+        System.out.println(ans);
     }
 
     static void subset(String processed,String unprocessed,int index){
@@ -21,4 +24,21 @@ public class Subset {
         subset(processed,unprocessed,index+1);
 
     }
+
+    static String[] subset(String processed,String unprocessed,int index,ArrayList<String> ans){
+        if(index>=unprocessed.length()){
+            ans.add(processed);
+            return ans.toArray(new String[0]);
+        }
+
+        char c = unprocessed.charAt(index);
+
+        String takeIt = processed + c;
+
+        subset(takeIt,unprocessed,index+1,ans);
+        subset(processed,unprocessed,index+1,ans);
+
+        return ans.toArray(new String[0]);
+    }
+
 }
