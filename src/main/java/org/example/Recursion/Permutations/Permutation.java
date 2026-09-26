4200c39a1cd775a8218dade390331b462f8c6c9f
krishnaaarwal <agrkrishna123@gmail.com>
2026-09-26 22:57:08 +0530

    Permutations using recursions

---
+++ src/main/java/org/example/Recursion/Permutations/Permutation.java
@@ -0,0 +1,77 @@
+package org.example.Recursion.Permutations;
+
+import java.util.ArrayList;
+
+public class Permutation {
+    public static void main(String[] args) {
+        System.out.println((permutation("","abc",0)));
+
+        ArrayList<String> ans = new ArrayList<>();
+        permutation("","abc",0,ans);
+        System.out.println(ans);
+    }
+
+    //Printing
+//    static void permutation(String processed,String unprocessed,int index){
+//        if(index >=unprocessed.length()){
+//            System.out.println(processed);
+//            return;
+//        }
+//
+//        int n = processed.length() + 1;
+//        char c = unprocessed.charAt(index);
+//
+//        for(int i=0;i<n;i++){
+//            String first = processed.substring(0,i);
+//            String second = processed.substring(i,processed.length());
+//
+//            String permutation =  first + c + second;
+//            permutation(permutation,unprocessed,index+1);
+//        }
+//    }
+
+    // Passing ArrayList as argument
+    static ArrayList<String> permutation(String processed, String unprocessed, int index, ArrayList<String> ans){
+        if(index >=unprocessed.length()){
+              ans.add(processed);
+              return ans;
+        }
+
+        int n = processed.length() + 1;
+        char c = unprocessed.charAt(index);
+
+        for(int i=0;i<n;i++){
+            String first = processed.substring(0,i);
+            String second = processed.substring(i,processed.length());
+
+            String permutation =  first + c + second;
+            permutation(permutation,unprocessed,index+1,ans);
+        }
+        return ans;
+    }
+
+    //In body ArrayList
+    static ArrayList<String> permutation(String processed,String unprocessed,int index){
+        if(index >=unprocessed.length()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        int n = processed.length() + 1;
+        char c = unprocessed.charAt(index);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            String first = processed.substring(0,i);
+            String second = processed.substring(i,processed.length());
+
+            String permutation =  first + c + second;
+            ArrayList<String> prev = permutation(permutation,unprocessed,index+1);
+
+            ans.addAll(prev);
+        }
+        return ans;
+    }
+}
